03ec81127f3f4f38e4d4e22eb62376d10e4a97c1
Alim4ik322 <assanovali23@gmail.com>
2024-12-04 22:13:46 +0300

    gt_7_3

--- testMongoose.js
+++ testMongoose.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-var schema = mongoose.Schema({ name: String })
-schema.methods.creaking = function(){
-    console.log(this.name + " - находятся в Литве")
-    }
-const Gate = mongoose.model('Gate', schema);
-const  collar_ring = new Gate({ name: 'Ворота Рассвета' });
-collar_ring.save().then(() => collar_ring.creaking());
+var Gate = require('./models/Gate.js').Gate
+var gate = new Gate({
+title: "Бранденбургские ворота",
+nick:"Brandenburg_Gate"
+})
+gate.save();
