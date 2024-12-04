ccfc133304465d1d9efaea9b9dd966327f804439
Alim4ik322 <assanovali23@gmail.com>
2024-12-04 22:01:16 +0300

    gt_7_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-const Cat = mongoose.model('Gate', { name: String });
-const kitty = new Cat({ name: 'Ворота Рассвета' });
-kitty.save().then(() => console.log('И еще одни ворота'));
+var schema = mongoose.Schema({ name: String })
+schema.methods.creaking = function(){
+    console.log(this.name + " - находятся в Литве")
+    }
+const Gate = mongoose.model('Gate', schema);
+const  collar_ring = new Gate({ name: 'Ворота Рассвета' });
+collar_ring.save().then(() => collar_ring.creaking());
