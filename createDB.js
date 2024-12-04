a78d86bd08e7df2796cb3aa1197a3563124d8fc4
Alim4ik322 <assanovali23@gmail.com>
2024-12-04 18:39:23 +0300

    gt_6_3

--- createDB.js
+++ createDB.js
@@ -5,23 +5,23 @@ var data = require("./data.js").data;
 console.log(data)
 // Connection URL
 console.log(data)
-// // Connection URL
-// const url = 'mongodb://localhost:27017';
-// const client = new MongoClient(url);
-// // Database Name
-// const dbName = 'gate_2024';
-// async function main() {
-// // Use connect method to connect to the server
-// await client.connect();
-// console.log('Connected successfully to server');
-// const db = client.db(dbName);
-// const collection = db.collection('documents');
-// // the following code examples can be pasted here...
-// const insertResult = await collection.insertMany([{ a: 1 }, {a: 2 }, { a: 3 }]);
-// console.log('Inserted documents =>', insertResult);
-// return 'done.';
-// }
-// main()
-// .then(console.log)
-// .catch(console.error)
-// .finally(() => client.close());
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+// Database Name
+const dbName = 'gate_2024';
+async function main() {
+// Use connect method to connect to the server
+await client.connect();
+console.log('Connected successfully to server');
+const db = client.db(dbName);
+const collection = db.collection('gates');
+// the following code examples can be pasted here...
+const insertResult = await collection.insertMany(data);
+console.log('Inserted documents =>', insertResult);
+return 'done.';
+}
+main()
+.then(console.log)
+.catch(console.error)
+.finally(() => client.close());
