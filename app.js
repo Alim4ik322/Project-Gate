9319dc7fca7c23de057b6455d6256746c253ea3c
Alim4ik322 <assanovali23@gmail.com>
2024-12-05 09:55:11 +0300

    gt_8_2

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ mongoose.connect('mongodb://localhost/tc2024')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var gates = require('./routes/gates');
+
 
 var app = express();
 
@@ -25,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/gates', gates);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
