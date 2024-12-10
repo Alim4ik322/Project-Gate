2c4d5c9c0d9a2efadecfca219c1c8ac07e31b4c1
Alim4ik322 <assanovali23@gmail.com>
2024-12-10 15:24:00 +0300

    gt_10_8

--- routes/gates.js
+++ routes/gates.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var Gate = require('../models/Gate').Gate;
-
+var checkAuth = require("../middlewares/checkAuth.js");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с gates');
 });
-router.get("/:nick", async function(req, res, next) {
+router.get("/:nick",checkAuth, async function(req, res, next) {
   var gates = await Gate.find({nick: req.params.nick});
   console.log(gates)
   if(!gates.length) return next(new Error("я таких ворот не знаю"))
