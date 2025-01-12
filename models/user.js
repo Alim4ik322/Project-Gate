a82e50e76d1ad0b3ab87c56162f01ee829f99509
Alim4ik322 <assanovali23@gmail.com>
2025-01-12 12:09:05 +0300

    gt_10_4

--- models/user.js
+++ models/user.js
@@ -26,10 +26,13 @@ userSchema.virtual("password").set(function(password){
     this._purePassword = password
     this.salt = Math.random() + ""
     this.hashedPassword = this.encryptPassword(password)
-    }).get(function(){
+}).get(function(){
     return this._purePassword
-    })
-    userSchema.methods.encryptPassword = function(password){
+})
+userSchema.methods.encryptPassword = function(password){
     return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
+}
+userSchema.methods.checkPassword = function(password){
+    return this.encryptPassword(password) === this.hashedPassword
 }
 module.exports.User = mongoose.model("User", userSchema)
